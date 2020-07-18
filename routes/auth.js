757c105d7f0f917f70ca800babde5a8418596ee9
dishadutta <duttadisha12345@gmail.com>
2020-07-19 01:05:41 +0530

    New Commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { signup, signin, signout, forgotPassword, resetPassword } = require("../controllers/auth");
+
+const { userById } = require("../controllers/user");
+const { userSignupValidator, passwordResetValidator } = require("../validator");
+
+const router = express.Router();
+
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+
+// password forgot and reset routes
+router.put("/forgot-password", forgotPassword);
+router.put("/reset-password", passwordResetValidator, resetPassword);
+
+
+//any route containing userId oue app will first execute userById
+router.param("userId", userById);
+
+module.exports = router;
